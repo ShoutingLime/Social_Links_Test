4dfd5e0ed2d1dbfe5bb84b04b7a0e521bcb89281
Мишутин Олег <mishutin@ren-tv.com>
2017-05-02 17:39:16 +0300

    26th commit

--- js/common.js
+++ js/common.js
@@ -1,13 +1,13 @@
-/*
-(setTimeout (function () {
+(function () {
   'use strict';
   var fbBlock = document.querySelector('.inlineBlock _2tga _49ve'),
-    shareWord = document.querySelector('#u_0_2 > div > span._49vh._2pi7');
+    shareWord = document.querySelector('#u_0_2 > div > span._49vh._2pi7'),
+    shareNumber = document.querySelector('#share[data-layout]');
 
   fbBlock.style.width = '50px';
   fbBlock.style.height = '62px';
   fbBlock.style.backgroundColor = '#fff';
   fbBlock.style.border = '1px solid gray';
   shareWord.style.display = 'none';
-}()), 3000);
-*/
+  console.log(shareNumber);
+}());
