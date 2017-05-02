e300070d4ab9bc06d33839456753e6f371528326
Мишутин Олег <mishutin@ren-tv.com>
2017-05-02 16:16:40 +0300

    17th commit

---
+++ js/common.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+  var fbBlock = document.querySelector('.inlineBlock _2tga _49ve'),
+    shareWord = document.querySelector('._49vh _2pi7');
+
+  fbBlock.style.width = '50px';
+  fbBlock.style.height = '62px';
+  fbBlock.style.backgroundColor = '#fff';
+  fbBlock.style.border = '1px solid gray';
+  shareWord.style.display = 'none';
+}());
